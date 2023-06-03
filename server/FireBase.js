b25d0f37ef16d114a7eae66a9540ed78c29a5965
vipul <vipularora180390@gmail.com>
2023-06-03 13:24:26 +0530

    source changed

--- server/FireBase.js
+++ server/FireBase.js
@@ -1,9 +1,9 @@
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getStorage } from "firebase/storage";
+const  { initializeApp } = require("firebase/app");
+const { getAnalytics } = require ("firebase/analytics");
+const { getStorage } = require ("firebase/storage");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
