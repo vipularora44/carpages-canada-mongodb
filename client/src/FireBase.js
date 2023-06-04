4d0da6d940994b22857dec080a096a60a370425d
vipul <vipularora180390@gmail.com>
2023-06-04 21:37:40 +0530

    firebase

--- client/src/FireBase.js
+++ client/src/FireBase.js
@@ -21,6 +21,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+//export const analytics = getAnalytics(app);
 export const storage = getStorage(app);
-export default app;
+//export default app;
