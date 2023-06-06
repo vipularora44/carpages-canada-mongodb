432c2ac58264f12eba9cb9c8eda2b4b451c56976
vipul <vipularora180390@gmail.com>
2023-06-06 20:10:04 +0530

    neWImages.length

--- client/src/MyComponents/SearchResults.js
+++ client/src/MyComponents/SearchResults.js
@@ -180,7 +180,7 @@ export default function SearchResults() {
         
           for(let i=0; i<neWImages.length; i++)
           {
-            
+            console.log("neWImages.length"+neWImages.length);
           let  url = await  storage().ref("/images/listing_images/"+fetchListings[i].All_Listings[0].image_name.toString()).getDownloadURL();
           console.log("Async func 2..."+url);
           setfetchImages(fetchImages=>[...fetchImages,url])
