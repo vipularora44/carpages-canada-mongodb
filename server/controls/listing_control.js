4edf49a869419823e77859b3caad615d0ab25f7c
vipul <vipularora180390@gmail.com>
2023-06-19 19:58:50 +0530

    {"image_type":"primary"}

--- server/controls/listing_control.js
+++ server/controls/listing_control.js
@@ -589,7 +589,7 @@ const getFilterListings=async(req,res)=>
                         from:"listings__images",
                         localField:"listing_id",
                         foreignField:"listing_id",
-                        pipeline:[ {$match:!withPictures?{"image_type":"primary"}:{"image_type":"primary","image_name":{$ne:imageName}}}],
+                        pipeline:[ {$match:!withPictures?{"image_type":"primary"}:{"image_type":"primary","image_name":{$ne:imageName.toString()}}}],
                         as:"All_Listings"
                     }
                 },
