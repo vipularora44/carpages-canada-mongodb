1aabb82efb7c542e1a6e0d1bd1b2941cfa059617
vipul <vipularora180390@gmail.com>
2023-06-20 17:15:03 +0530

    Bahia Encerrada

--- server/controls/listing_control.js
+++ server/controls/listing_control.js
@@ -601,7 +601,7 @@ const getFilterListings=async(req,res)=>
                         from:"listings__images",
                         localField:"listing_id",
                         foreignField:"listing_id",
-                        pipeline:[ { $match:{"image_type":"primary"}}],
+                        pipeline:[ { $match:conditions2}],
                         as:"All_Listings"
                     }
                 },
